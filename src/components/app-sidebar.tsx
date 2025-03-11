6a1ffb8dc7ca50fb6a2242e1ba14c07a7b7dbd4a
Antony Perez <antonyperez0@yahoo.com>
2025-03-11 07:39:00 -0700

    added sidebar highlight

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,7 +1,6 @@
 import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
-import { useTheme } from "./theme-provider";
 
 import {
   Sidebar,
@@ -44,6 +43,8 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const location = useLocation();
+
   return (
     <Sidebar>
       <SidebarContent>
@@ -59,7 +60,14 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton
+                    asChild
+                    isActive={
+                      location.pathname === item.url ||
+                      (item.url !== "/" &&
+                        location.pathname.startsWith(item.url))
+                    }
+                  >
                     <Link to={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
